6be80c2a8bf4c0993f2868c06952c5d5d3933309
mthan.net <mthandotnet@gmail.com>
2026-03-05 03:00:57 +0700

    Wakanda Forever!

--- admin/metabox/page-metabox.php
+++ admin/metabox/page-metabox.php
@@ -11,6 +11,28 @@ if (class_exists('CSF')) {
         'context' => 'normal',
     ));
 
+    // Fetch available sections
+    $available_sections = array();
+    $theme_options = get_option('mthan_theme_options');
+    $sections_path = get_template_directory() . '/sections/';
+
+    if (is_dir($sections_path)) {
+        $files = glob($sections_path . '*.php');
+        if ($files) {
+            foreach ($files as $file) {
+                $filename = basename($file, '.php');
+                $option_id = 'enable_section_' . str_replace('-', '_', $filename);
+
+                // Defaults to true
+                $is_enabled = isset($theme_options[$option_id]) ? $theme_options[$option_id] : true;
+
+                if ($is_enabled) {
+                    $available_sections[$filename] = ucwords(str_replace('-', ' ', $filename));
+                }
+            }
+        }
+    }
+
     // Create a section
     CSF::createSection($prefix, array(
         'title' => 'General Settings',
@@ -37,6 +59,30 @@ if (class_exists('CSF')) {
                 'default' => true,
             ),
 
+        )
+    ));
+
+    CSF::createSection($prefix, array(
+        'title' => 'Page Builder (Sections)',
+        'icon' => 'fas fa-layer-group',
+        'fields' => array(
+                array(
+                'id' => 'page_sections',
+                'type' => 'group',
+                'title' => 'Sections',
+                'button_title' => 'Add New Section',
+                'accordion_title_auto' => false,
+                'accordion_title_prefix' => 'Section: ',
+                'accordion_title_number' => true,
+                'fields' => array(
+                        array(
+                        'id' => 'section_template',
+                        'type' => 'select',
+                        'title' => 'Select Template',
+                        'options' => empty($available_sections) ? array('' => 'No sections enabled') : $available_sections,
+                    ),
+                )
+            )
         )
     ));
 
