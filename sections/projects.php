f41f301422af3f66a652079553ed8975854c69d9
mthan.net <mthandotnet@gmail.com>
2026-03-05 23:10:24 +0700

    Wakanda Forever!

--- sections/projects.php
+++ sections/projects.php
@@ -195,13 +195,13 @@ function mthan_section_projects_html_1($section_data) {
     $lower         = !empty($section_data['lower_text']) ? $section_data['lower_text'] : '';
     $all_link      = !empty($section_data['view_all_link']) ? $section_data['view_all_link'] : '#';
     $fallback_imgs = array(
+        get_template_directory_uri() . '/assets/images/resource/featured-image-1.jpg',
         get_template_directory_uri() . '/assets/images/resource/featured-image-2.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/2.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/3.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/4.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/5.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/6.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/7.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-3.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-4.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-5.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-6.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-7.jpg',
     );
 ?>
 <section class="projects-section">
@@ -253,13 +253,13 @@ function mthan_section_projects_html_2($section_data) {
     $repeater      = !empty($section_data['projects_repeater']) ? $section_data['projects_repeater'] : array();
     $all_link      = !empty($section_data['view_all_link']) ? $section_data['view_all_link'] : '#';
     $fallback_imgs = array(
-        get_template_directory_uri() . '/assets/images/gallery/1.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/2.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/3.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/4.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/5.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/6.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/7.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-1.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-2.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-3.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-4.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-5.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-6.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-7.jpg',
     );
 ?>
 <section class="projects-two">
@@ -317,13 +317,13 @@ function mthan_section_projects_html_3($section_data) {
     $filters_repeater = !empty($section_data['filters_repeater']) ? $section_data['filters_repeater'] : array();
     $gallery_repeater = !empty($section_data['projects_repeater']) ? $section_data['projects_repeater'] : array();
     $fallback_imgs    = array(
-        get_template_directory_uri() . '/assets/images/gallery/1.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/2.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/3.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/4.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/5.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/6.jpg',
-        get_template_directory_uri() . '/assets/images/gallery/7.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-1.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-2.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-3.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-4.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-5.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-6.jpg',
+        get_template_directory_uri() . '/assets/images/resource/featured-image-7.jpg',
     );
 ?>
 <section class="gallery-section">
