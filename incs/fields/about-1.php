9e883cdb427440352e4f1066738111d6481557a4
mthan.net <mthandotnet@gmail.com>
2026-03-08 04:48:30 +0700

    1

--- incs/fields/about-1.php
+++ incs/fields/about-1.php
@@ -5,7 +5,6 @@
  */
 function mthan_section_about_1_options() {
     $slug = 'about-1';
-    $dependency = ['section_template', '==', $slug];
 
     return [
         [
@@ -13,14 +12,12 @@ function mthan_section_about_1_options() {
             'id'      => $slug . '_subtitle',
             'default' => 'About Us',
             'type'    => 'text',
-            'dependency' => $dependency,
         ],
         [
             'default' => 'Professional Gardener',
             'title'   => 'Section Title',
             'id'      => $slug . '_title',
             'type'    => 'text',
-            'dependency' => $dependency,
         ],
         [
             'default' => get_template_directory_uri() . '/assets/images/icons/leaf-two.png',
@@ -28,42 +25,36 @@ function mthan_section_about_1_options() {
             'id'      => $slug . '_subtitle_icon',
             'type'    => 'upload',
             'preview' => false,
-            'dependency' => $dependency,
         ],
         [
             'default' => '<p class="bigger-text">Leader in landscaping, lawn care, and irrigation services in Los Angeles since 2004.</p><p>Must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and we will give you a complete account of the system, and expound the actualy teachings.</p>',
             'id'      => $slug . '_about_content',
             'type'    => 'wp_editor',
             'title'   => 'Content',
-            'dependency' => $dependency,
         ],
         [
             'default'    => 'Our Company has established a reputation as the leader in landscape design.',
             'id'         => $slug . '_quote_text',
             'title'      => 'Quote Text',
             'type'       => 'textarea',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_quote_author',
             'type'       => 'text',
             'title'      => 'Quote Author',
             'default'    => 'Chris Stanley,',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_quote_designation',
             'type'       => 'text',
             'title'      => 'Quote Designation',
             'default'    => 'Founder of Pruners',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_signature_image',
             'type'       => 'upload',
             'preview'    => false,
             'title'      => 'Signature Image',
-            'dependency' => $dependency,
         ],
         [
             'id'    => $slug . '_image',
@@ -71,7 +62,6 @@ function mthan_section_about_1_options() {
             'preview' => false,
             'title' => 'Main Image',
             'default' => get_template_directory_uri() . '/assets/images/resource/featured-image-1.jpg',
-            'dependency' => $dependency,
         ],
         [
             'id'    => $slug . '_anim_image',
@@ -79,13 +69,11 @@ function mthan_section_about_1_options() {
             'preview' => false,
             'title' => 'Animated Image (Small)',
             'default' => get_template_directory_uri() . '/assets/images/resource/anim-image-1.png',
-            'dependency' => $dependency,
         ],
         [
             'id'    => $slug . '_video_url',
             'type'  => 'text',
             'title' => 'Video URL',
-            'dependency' => $dependency,
         ],
         [
             'id'      => $slug . '_quote_thumb',
@@ -93,30 +81,26 @@ function mthan_section_about_1_options() {
             'preview' => false,
             'title'   => 'Video Thumbnail Image',
             'default' => get_template_directory_uri() . '/assets/images/resource/quote-thumb.jpg',
-            'dependency' => $dependency,
         ],
-        array_merge(mthan_btn_text_field('Read More', 'Button Text', $slug . '_btn_text'), ['dependency' => $dependency]),
-        array_merge(mthan_btn_link_field('#', 'Button Link', $slug . '_btn_link'), ['dependency' => $dependency]),
+        mthan_btn_text_field('Read More', 'Button Text', $slug . '_btn_text'),
+        mthan_btn_link_field('#', 'Button Link', $slug . '_btn_link'),
         [
             'id'      => $slug . '_exp_count',
             'type'    => 'text',
             'title'   => 'Experience Years (Number)',
             'default' => '25',
-            'dependency' => $dependency,
         ],
         [
             'id'      => $slug . '_exp_text',
             'type'    => 'textarea',
             'title'   => 'Experience Text',
             'default' => 'Years of <br>Experience',
-            'dependency' => $dependency,
         ],
         [
             'id'      => $slug . '_exp_icon',
             'type'    => 'text',
             'title'   => 'Experience Icon Class',
             'default' => 'flaticon-leaves',
-            'dependency' => $dependency,
         ],
     ];
 }
--- incs/fields/about-3.php
+++ incs/fields/about-3.php
@@ -5,67 +5,58 @@
  */
 function mthan_section_about_3_options() {
     $slug = 'about-3';
-    $dependency = ['section_template', '==', $slug];
 
     return [
-        array_merge(mthan_subtitle_field('About Us'), ['id' => $slug . '_subtitle', 'dependency' => $dependency]),
+        array_merge(mthan_subtitle_field('About Us'), ['id' => $slug . '_subtitle']),
         [
             'id'      => $slug . '_subtitle_icon',
             'type'    => 'upload',
             'preview' => false,
             'title'   => 'Subtitle Icon',
             'default' => get_template_directory_uri() . '/assets/images/icons/leaf-two.png',
-            'dependency' => $dependency,
         ],
-        array_merge(mthan_title_field('Professional Gardener'), ['id' => $slug . '_title', 'dependency' => $dependency]),
+        array_merge(mthan_title_field('Professional Gardener'), ['id' => $slug . '_title']),
         [
             'id'      => $slug . '_about_content',
             'type'    => 'wp_editor',
             'title'   => 'Content',
             'default' => '<p class="bigger-text">Leader in landscaping, lawn care, and irrigation services in Los Angeles since 2004.</p><p>Must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and we will give you a complete account of the system, and expound the actualy teachings.</p>',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_quote_text',
             'type'       => 'textarea',
             'title'      => 'Quote Text',
             'default'    => 'Our Company has established a reputation as the leader in landscape design.',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_quote_author',
             'type'       => 'text',
             'title'      => 'Quote Author',
             'default'    => 'Chris Stanley,',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_quote_designation',
             'type'       => 'text',
             'title'      => 'Quote Designation',
             'default'    => 'Founder of Pruners',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_signature_image',
             'type'       => 'upload',
             'preview'    => false,
             'title'      => 'Signature Image',
-            'dependency' => $dependency,
         ],
         [
             'id'         => $slug . '_iso_number',
             'type'       => 'text',
             'title'      => 'ISO Number',
             'default'    => 'ISO 9001:2015',
-            'dependency' => $dependency,
         ],
         [
             'id'    => $slug . '_image',
             'type'  => 'upload',
             'preview' => false,
             'title' => 'Main Image',
-            'dependency' => $dependency,
         ],
     ];
 }
