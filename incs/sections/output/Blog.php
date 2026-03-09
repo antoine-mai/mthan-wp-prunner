148198eec289043772bda49b234269c5e0e404f6
mthan.net <mthandotnet@gmail.com>
2026-03-10 02:28:34 +0700

    Applied page/post selector to all link fields in sections and theme options

--- incs/sections/output/Blog.php
+++ incs/sections/output/Blog.php
@@ -23,7 +23,7 @@ function mthan_section_Blog_html($section_data) { ?>
     $default_thumb = mthan_sec_img(mthan_get_section_val($slug, $section_data, 'default_thumb', get_template_directory_uri() . '/assets/images/resource/news-image-1.jpg'));
     $show_header   = mthan_get_section_val($slug, $section_data, 'show_header', true);
 
-    $args = [;
+    $args = [
         'post_type'      => 'post',
         'posts_per_page' => $count,
         'paged'          => $paged,
